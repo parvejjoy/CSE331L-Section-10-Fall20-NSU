e07157952493ca086b5b3b3834554d494281a9e9
Parvejjoy <pervej.joy@northsouth.edu>
2020-11-11 23:14:55 +0600

    lab assesment uploaded

---
+++ Lab Assessment Submission/Lab 1/lab assesment 1.asm
@@ -0,0 +1,21 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+            
+
+            
+org 100h   
+  
+MOV AX, 1
+MOV BX, 5
+MOV DX, 5 
+
+ADD BX, DX
+ADD AX, BX  
+
+
+ret
+
+
+
+
